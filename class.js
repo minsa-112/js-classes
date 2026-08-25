3d281e40d3eba2b81af9d6c52b8c9b2301746c2f
Minsa ali <anou2651@gmail.com>
2026-08-25 18:54:04 +0500

    add a lect

---
+++ class.js
@@ -0,0 +1,29 @@
+// const per=90;
+// if(per>=80 && per<=100){
+//     console.log("A+");
+// }
+// else if(per>=70 && per<=79){
+//     console.log("A");
+// }
+// else if(per>=60 && per<=69){
+//     console.log("B");
+// }
+// else if(per>=50 && per<=59){
+//     console.log("C");
+// }
+// else if(per>=40 && per<=49){
+//     console.log("D");
+// }
+// else{
+//     console.log("Fail");
+// }
+const p=89;
+if(p>=90 && p<=100){
+    console.log("A++")
+}
+else if (p>=80 && p<=79){
+    console.log("A+")
+}
+else if (p>=78 && p<=60){
+    console.log("B")
+}
