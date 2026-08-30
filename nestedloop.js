b36704bf9a6c76f5ea81f9605ddd4cd955bf849f
Minsa ali <anou2651@gmail.com>
2026-08-30 11:59:36 +0500

    add a let

--- nestedloop.js
+++ nestedloop.js
@@ -1,14 +1,35 @@
-let check="hi this is my class of nested loop"
-let vowel=0;
-let cons=0;
-for (let teststr of check.toLowerCase()){
-    if("aeiou" .includes(teststr))vowel++;
-    else if(teststr>='a' && teststr<='z') cons++;
-}
-console.log("vowels",vowel,"consonant", cons,);
+// let check="hi this is my class of nested loop"
+// let vowel=0;
+// let cons=0;
+// for (let teststr of check.toLowerCase()){
+//     if("aeiou" .includes(teststr))vowel++;
+//     else if(teststr>='a' && teststr<='z') cons++;
+// }
+// console.log("vowels",vowel,"consonant", cons,);
 
-for(let a=0; a<10;a++){
-    for(let b=0;b<10;b++){
-        console.log(a+b)
+// for(let a=0; a<10;a++){
+//     for(let b=0;b<10;b++){
+//         console.log(a+b)
+//     }
+// }
+
+let w="5";
+for(let a=1;a<=5;a++){
+    let hint="";
+    for(let b=1;b<=5;b++){
+        if(b<=a){
+            hint+="#"
+        }
     }
+    console.log(hint)
+}
+let s="5";
+for(let e=1;e<=5;e++){
+    let output="";
+    for(let d=1;d<=5;d++){
+        if(d>=e){
+            output+="#"
+    }
+}
+    console.log(output)
 }
